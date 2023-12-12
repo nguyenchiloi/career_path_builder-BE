39530bcdfa134f0528248b5c7bedfd95d6099fb7
Nguyen Xuan Tien <nguyenxuantien20062001@gmail.com>
2023-12-12 14:55:12 +0700

    xuantien

--- lib/demo_model/obj/Debug/net7.0/demo_model.AssemblyInfo.cs
+++ lib/demo_model/obj/Debug/net7.0/demo_model.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("demo_model")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36f6deef6c5d4ae50cd58a55f43e5ae7a27fb76d")]
 [assembly: System.Reflection.AssemblyProductAttribute("demo_model")]
 [assembly: System.Reflection.AssemblyTitleAttribute("demo_model")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
